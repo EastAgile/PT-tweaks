3911c0174ed1db357ec513bd17bbe26d084ebad2
ea-open-source <open-source@eastagile.com>
2019-01-28 10:13:30 +0700

    [f] 513 - Add docker development
    [#163318513] https://www.pivotaltracker.com/story/show/163318513

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,25 @@
 from .base import *  # noqa: F401, F403
 
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASSWORD'),
+        'HOST': env.str('DATABASE_HOST'),
+    }
+}
+
+# Auth
+# ------------------------------------------------------------------------------
+AUTH_PASSWORD_VALIDATORS = []
+
+
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
